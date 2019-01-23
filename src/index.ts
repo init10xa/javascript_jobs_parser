7d66619c69ca33c51b9a3f55ad88035578de25e6
nikfakel <nikfakel@gmail.com>
2019-01-24 02:21:52 +0300

    Change heroku PORT

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-var express = require('express');
-var app = express();
-var port = 8080;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
 
 // start the server
-app.listen(port, function() {
+app.listen(port, () => {
   console.log('app started');
 });
 
 // route our app
-app.get('/', function(req: any, res: any) {
+app.get('/', (req: any, res: any) => {
   console.log(req, res);
   res.send('hello world!');
 });
